9f58bee9950cdc6f62de57627166a23dd7190660
xu <2137746787@qq.com>
2024-01-29 17:40:52 +0800

    显示评论

--- src/main/java/com/nowcoder/community/controller/DiscussPostController.java
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -1,9 +1,13 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.entity.Comment;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.Page;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.CommentService;
 import com.nowcoder.community.service.DiscussPostService;
 import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Date;
+import java.util.*;
 
 @Controller
 @RequestMapping("/discuss")
-public class DiscussPostController {
+public class DiscussPostController implements CommunityConstant {
     @Autowired
     private DiscussPostService discussPostService;
 
@@ -27,6 +31,9 @@ public class DiscussPostController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private CommentService commentService;
 
     @RequestMapping(path="/add",method = RequestMethod.POST)
     @ResponseBody
@@ -49,7 +56,7 @@ public class DiscussPostController {
 
 
     @RequestMapping(path="/detail/{discussPostId}",method = RequestMethod.GET)
-    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model){
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model, Page page){
         // 1.根据帖子的id查询帖子的详情
         DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
         // 2.将帖子的详情传给前端
@@ -58,6 +65,57 @@ public class DiscussPostController {
         User user = userService.findUserById(post.getUserId());
         // 4.将帖子的作者传给前端
         model.addAttribute("user",user);
+
+        // 5.评论的分页信息
+        page.setLimit(5);
+        page.setPath("/discuss/detail/"+discussPostId);
+        page.setRows(post.getCommentCount());
+
+        // 6.查询帖子的评论
+        // 评论 : 给帖子的评论
+        // 回复 : 给评论的评论
+        // 评论的列表
+        List<Comment> commentList = commentService.findCommentsByEntity(ENTITY_TYPE_POST, post.getId(), page.getOffset(), page.getLimit());
+        // 评论的VO列表
+        List<Map<String,Object>> commentVoList = new ArrayList<>();
+        if(commentList !=null){
+            for(Comment comment:commentList){
+                // 评论的VO
+                Map<String,Object> commentVo = new HashMap<>();
+                // 评论
+                commentVo.put("comment",comment);
+                // 评论的作者
+                commentVo.put("user",userService.findUserById(comment.getUserId()));
+
+                // 评论的回复列表
+                List<Comment> replyList = commentService.findCommentsByEntity(
+                        ENTITY_TYPE_COMMENT, comment.getId(), 0, Integer.MAX_VALUE);
+                // 回复的VO列表
+                List<Map<String,Object>> replyVoList = new ArrayList<>();
+                if(replyList !=null){
+                    for(Comment reply:replyList){
+                        Map<String,Object> replyVo = new HashMap<>();
+                        // 回复
+                        replyVo.put("reply",reply);
+                        // 回复的作者
+                        replyVo.put("user",userService.findUserById(reply.getUserId()));
+                        // 回复的目标
+                        // 只出现在回复的回复中，即回复的回复才有目标
+                        // 评论的回复不会有目标
+                        User target = reply.getTargetId() == 0 ? null : userService.findUserById(reply.getTargetId());
+                        replyVo.put("target",target);
+                        replyVoList.add(replyVo);
+                    }
+                }
+                commentVo.put("replys",replyVoList);
+
+                // 回复的数量
+                int replyCount = commentService.findCommentCount(ENTITY_TYPE_COMMENT, comment.getId());
+                commentVo.put("replyCount",replyCount);
+                commentVoList.add(commentVo);
+            }
+        }
+        model.addAttribute("comments",commentVoList);
         // 5.返回帖子详情的页面
         return "/site/discuss-detail";
     }
--- src/main/java/com/nowcoder/community/util/CommunityConstant.java
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -23,4 +23,15 @@ public interface CommunityConstant {
      * 记住状态下的 登录凭证超时时间
      */
     int REMEMBER_EXPIRED_SECOND = 3600 * 24 * 1000;
+
+    /**
+     * 实体类型：帖子
+     */
+    int ENTITY_TYPE_POST = 1;
+
+    /**
+     * 实体类型：评论
+     */
+    int ENTITY_TYPE_COMMENT = 2;
+
 }
